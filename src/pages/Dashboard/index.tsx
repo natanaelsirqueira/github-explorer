d1886ea9868c92ab627a0f69e58d469931bb071a
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-05-09 15:30:05 -0400

    Fetching repositories from the API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,63 +1,69 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/13697898?s=460&u=2aedada2b840e9a047fbec5b49fc8e0d3e5f7236&v=4"
-            alt="Natanael Sirqueira"
-          />
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="test">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>natanaelsirqueira/BeTheHero</strong>
-            <p>Projeto desenvolvido durante a Semana OmniStack 11.0</p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/13697898?s=460&u=2aedada2b840e9a047fbec5b49fc8e0d3e5f7236&v=4"
-            alt="Natanael Sirqueira"
-          />
-
-          <div>
-            <strong>natanaelsirqueira/BeTheHero</strong>
-            <p>Projeto desenvolvido durante a Semana OmniStack 11.0</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="test">
-          <img
-            src="https://avatars1.githubusercontent.com/u/13697898?s=460&u=2aedada2b840e9a047fbec5b49fc8e0d3e5f7236&v=4"
-            alt="Natanael Sirqueira"
-          />
-
-          <div>
-            <strong>natanaelsirqueira/BeTheHero</strong>
-            <p>Projeto desenvolvido durante a Semana OmniStack 11.0</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
